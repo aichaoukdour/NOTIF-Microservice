fb00f619729faf3597d61635ef06061394749529
aichaoukdour <147880095+aichaoukdour@users.noreply.github.com>
2025-04-22 20:58:27 +0200

    cleaning codes

--- src/main/java/com/example/NotificationService/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/NotificationService/exception/GlobalExceptionHandler.java
@@ -5,29 +5,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-/**
- * Global exception handler for custom exceptions.
- */
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    /**
-     * Handles TemplateNotFoundException.
-     *
-     * @param ex the exception
-     * @return ResponseEntity with error message and HTTP status
-     */
     @ExceptionHandler(TemplateNotFoundException.class)
     public ResponseEntity<String> handleTemplateNotFoundException(TemplateNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
     }
 
-    /**
-     * Handles EmailSendingException.
-     *
-     * @param ex the exception
-     * @return ResponseEntity with error message and HTTP status
-     */
     @ExceptionHandler(EmailSendingException.class)
     public ResponseEntity<String> handleEmailSendingException(EmailSendingException ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
